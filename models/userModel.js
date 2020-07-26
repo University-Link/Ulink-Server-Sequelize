2c89d95d4c9a7ea60d451e95597cf5b7fbba536b
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-07-26 09:48:56 +0900

    fix : Update Uesr Table
    ? : User Table column 수정
    - refresh_token 길이 3000에서 500으로 수정

--- models/userModel.js
+++ models/userModel.js
@@ -1,57 +1,56 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("user", {
-    userIdx: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    id: {
-      type: DataTypes.STRING(100),
-      unique: true,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING(45),
-    },
-    password: {
-      type: DataTypes.STRING(200),
-      allowNull: false,
-    },
-    salt: {
-      type: DataTypes.STRING(200),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(50),
-      unique: true,
-    },
-    phone: {
-      type: DataTypes.STRING(50),
-      unique: true,
-    },
-    nickname: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      unique: true,
-    },
-    check: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: 0,
-    },
-    refresh_token: {
-      type: DataTypes.STRING(1000),
-    },
-    fcm_token: {
-      type: DataTypes.STRING(3000),
-    },
-    profileImage: {
-      type: DataTypes.STRING(200),
-    },
-    gender: {
-      type: DataTypes.STRING(30),
-    },
-  }, {
-    freezeTableName: true,
-    timestamps: true,
-  });
+    return sequelize.define("user", {
+        userIdx: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING(100),
+            unique: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(45),
+        },
+        password: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        salt: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            unique: true,
+        },
+        phone: {
+            type: DataTypes.STRING(50),
+            unique: true,
+        },
+        nickname: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        check: { // 학교 인증 했는지 안했는지
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0,
+        },
+        refresh_token: {
+            type: DataTypes.STRING(500),
+        },
+        fcm_token: {
+            type: DataTypes.STRING(3000),
+        },
+        profileImage: {
+            type: DataTypes.STRING(200),
+        },
+        gender: {
+            type: DataTypes.STRING(30),
+        },
+    }, {
+        freezeTableName: true,
+        timestamps: true,
+    });
 };
